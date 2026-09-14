94a715b25cc8b3c5985b3d77ff23a691f7fee1b4
XchenDev <Pxczxn@163.com>
2026-09-15 03:33:49 +0800

    【feat】接入安全启动确认命令

--- src-tauri/src/tauri_api/mod.rs
+++ src-tauri/src/tauri_api/mod.rs
@@ -1,7 +1,9 @@
 mod commands;
+mod launch_confirmation;
 mod launch_profiles;
 mod projects;
 
 pub use commands::*;
+pub use launch_confirmation::*;
 pub use launch_profiles::*;
 pub use projects::*;
