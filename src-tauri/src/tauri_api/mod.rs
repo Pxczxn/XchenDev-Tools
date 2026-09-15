a2d52712cdec617e7a31f1228dcec23aa1929ef1
XchenDev <Pxczxn@163.com>
2026-09-15 14:49:03 +0800

    【security】注册安全配置导入模块

--- src-tauri/src/tauri_api/mod.rs
+++ src-tauri/src/tauri_api/mod.rs
@@ -1,10 +1,12 @@
 mod commands;
+mod config_import;
 mod launch_confirmation;
 mod launch_profiles;
 mod projects;
 mod runtime_items;
 
 pub use commands::*;
+pub use config_import::*;
 pub use launch_confirmation::*;
 pub use launch_profiles::*;
 pub use projects::*;
